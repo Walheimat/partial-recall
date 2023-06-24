58c337f144e618bf15e0df4bb5ac5c09a5286888
Walheimat <krister.schuchardt@gmail.com>
2023-06-24 09:30:19 +0200

    chore: add sibling rules

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,3 @@
-((nil . ((wal-project-test-default-cmd . "make local-test"))))
+((nil . ((wal-project-test-default-cmd . "make local-test")
+         (find-sibling-rules . (("test/\\([^/]+\\)-test.el" "\\1.el")
+                                ("\\([^/]+\\).el" "test/\\1-test.el"))))))
