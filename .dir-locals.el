badac457bc953dbdc54e98e7b3ac8af1178d1dea
Walheimat <krister.schuchardt@gmail.com>
2023-06-24 17:19:57 +0200

    chore: update dinghy

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((wal-project-test-default-cmd . "make local-test")
+((nil . ((wal-project-install-default-cmd . "make project-install")
          (find-sibling-rules . (("test/\\([^/]+\\)-test.el" "\\1.el")
                                 ("\\([^/]+\\).el" "test/\\1-test.el"))))))
