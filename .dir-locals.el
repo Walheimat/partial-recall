61191819b77932ea3d5f2d09838fd1900b73ce71
Walheimat <krister.schuchardt@gmail.com>
2023-07-06 17:44:49 +0200

    fix(makefile): install target

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,3 @@
-((nil . ((wal-project-install-default-cmd . "make project-install")
+((nil . ((wal-project-install-default-cmd . "make package-install")
          (find-sibling-rules . (("test/\\([^/]+\\)-test.el" "\\1.el")
                                 ("\\([^/]+\\).el" "test/\\1-test.el"))))))
