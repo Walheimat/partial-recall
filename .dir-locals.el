58f763c5265269eda593c53709f5088f92dcbcc6
Walheimat <krister.schuchardt@gmail.com>
2023-08-18 08:46:37 +0200

    chore(makefile): update dinghy

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,4 +1,5 @@
 ((nil . ((wal-project-install-default-cmd . "make package-install")
-         (wal-project-execute-default-cmd . "make update-version")
+         (wal-project-install-reverse-mode . t)
+         (wal-project-execute-default-cmd . ("make update-version" "make pacify"))
          (find-sibling-rules . (("test/\\([^/]+\\)-test.el" "\\1.el")
                                 ("\\([^/]+\\).el" "test/\\1-test.el"))))))
--- Makefile
+++ Makefile
@@ -2,4 +2,8 @@ PACKAGE_NAME=partial-recall
 PACKAGE_SUFFIX=tar
 CURRENT_PACKAGE_VERSION=0.6.4
 
+PACIFY_DEPS=partial-recall.el \
+			partial-recall-menu.el \
+			partial-recall-extensions.el
+
 include dinghy/emacs-package.mk
