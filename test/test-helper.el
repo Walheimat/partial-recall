aeea425280e4fe1355804c3e0375fff2479d7f6d
Walheimat <krister.schuchardt@gmail.com>
2023-08-27 15:51:58 +0200

    refac: re-use hash table and buffer in helper

--- test/test-helper.el
+++ test/test-helper.el
@@ -13,29 +13,39 @@
 ;; Helpers
 
 (defvar test-tab '(current-tab (name . "test-tab") (explicit-name . t) (pr . "test-hash")))
+(defvar test-table (make-hash-table))
+(defvar test-tabs (list test-tab))
 
 (cl-defmacro with-tab-history (&rest body &key pre lifts second &allow-other-keys)
-  "Run BODY with a clear tab history and a temp buffer.
+  "Execute BODY in a clean environment.
+
+This environment is a clear tab history and a single existing
+static tab.
 
 If PRE is t, pre-remember the current buffer. Unless LIFTS is t,
 ignore calls to `partial-recall--lift'. If SECOND is true, a
 second memory is created."
   (declare (indent defun))
+
   `(bydi ((:mock tab-bar--current-tab :return test-tab)
-          (:mock tab-bar-tabs :return (list test-tab))
+          (:mock tab-bar-tabs :return test-tabs)
           (:always partial-recall--meaningful-buffer-p)
           ,(unless lifts '(:ignore partial-recall--lift)))
-     (let* ((partial-recall--table (make-hash-table))
-            (second-memory-key "second")
-            (second-memory nil))
 
-       (with-temp-buffer
-         ,(when pre '(partial-recall--remember (current-buffer)))
-         ,(when second
-            '(progn
-               (setq second-memory (partial-recall--memory-create second-memory-key))
-               (puthash second-memory-key second-memory partial-recall--table)))
-         ,@body))))
+     (let ((partial-recall--table test-table)
+           (second-memory-key "second")
+           (second-memory nil))
+
+       ,(when pre '(partial-recall--remember (current-buffer)))
+
+       ,(when second
+          '(progn
+             (setq second-memory (partial-recall--memory-create second-memory-key))
+             (puthash second-memory-key second-memory partial-recall--table)))
+
+       (unwind-protect
+           ,@body
+         (clrhash test-table)))))
 
 ;; Setup
 
