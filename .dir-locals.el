17e6204e6ccda5829047eebb0ffa5e731306aef9
Walheimat <krister.schuchardt@gmail.com>
2023-09-13 21:05:56 +0200

    fix(dir-locals): remove dead variable

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,5 +1,4 @@
 ((nil . ((wal-project-install-default-cmd . "make package-install")
-         (wal-project-install-reverse-mode . t)
          (wal-project-execute-default-cmd . ("make update-version" "make pacify"))
          (find-sibling-rules . (("test/\\([^/]+\\)-test.el" "\\1.el")
                                 ("\\([^/]+\\).el" "test/\\1-test.el"))))))
