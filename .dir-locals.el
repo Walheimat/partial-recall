1f1f1a014b48c96591dd58f92021f14a806db9a1
Walheimat <krister.schuchardt@gmail.com>
2023-09-25 19:12:37 +0200

    chore: update ignores

--- .dir-locals.el
+++
@@ -1,4 +0,0 @@
-((nil . ((wal-project-install-default-cmd . "make package-install")
-         (wal-project-execute-default-cmd . ("make update-version" "make pacify"))
-         (find-sibling-rules . (("test/\\([^/]+\\)-test.el" "\\1.el")
-                                ("\\([^/]+\\).el" "test/\\1-test.el"))))))
